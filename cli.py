1b2d9443e50e80695dcc49fbc78ee7b21ba88e95
Phuc Doan <phucxdoan@gmail.com>
2026-01-04 12:26:21 -0500

    Added `pxd` submodule.

--- cli.py
+++ cli.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python3
 
-from pxd import logger, make_main_relative_path, execute_shell_command, CommandLineInterface
+import deps.pxd.pxd as pxd
 
 
 
@@ -8,7 +8,7 @@ from pxd import logger, make_main_relative_path, execute_shell_command, CommandL
 
 
 
-main_interface = CommandLineInterface()
+main_interface = pxd.CommandLineInterface()
 
 
 
@@ -24,12 +24,12 @@ main_interface = CommandLineInterface()
 def clean(parameters):
 
     DIRECTORIES = (
-        make_main_relative_path('./build'),
+        pxd.make_main_relative_path('./build'),
     )
 
     for directory in DIRECTORIES:
 
-        execute_shell_command(
+        pxd.execute_shell_command(
             bash = f'''
                 rm -rf {repr(directory.as_posix())}
             ''',
@@ -51,20 +51,20 @@ def clean(parameters):
 )
 def build(parameters):
 
-    execute_shell_command(
-        bash = f'mkdir -p {make_main_relative_path('./build')}',
+    pxd.execute_shell_command(
+        bash = f'mkdir -p {pxd.make_main_relative_path('./build')}',
         cmd  = f'''
-            if not exist "{make_main_relative_path('./build')}" (
-                mkdir {make_main_relative_path('./build')}
+            if not exist "{pxd.make_main_relative_path('./build')}" (
+                mkdir {pxd.make_main_relative_path('./build')}
             )
         ''',
     )
 
-    execute_shell_command('''
+    pxd.execute_shell_command('''
         cd ./deps/raylib && zig build
     ''')
 
-    execute_shell_command('''
+    pxd.execute_shell_command('''
         cd build && zig cc ../source/ThunkWeb.c ../deps/raylib/zig-out/lib/raylib.lib -l gdi32 -l Winmm -o ./ThunkWeb.exe
     ''')
 
