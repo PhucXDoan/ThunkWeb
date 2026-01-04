1f74b8e6aa043acb2c9019a8a69e08a4e762ad04
Phuc Doan <phucxdoan@gmail.com>
2026-01-04 17:26:43 -0500

    Adding meta-preprocessor stage to build compilation.

--- cli.py
+++ cli.py
@@ -1,5 +1,8 @@
 #! /usr/bin/env python3
 
+
+
+import pathlib
 import deps.pxd.pxd as pxd
 
 
@@ -48,8 +51,48 @@ def clean(parameters):
     {
         'description' : 'Compile and generate the executable.',
     },
+    {
+        'name'        : 'metapreprocess_only',
+        'description' : 'Run the meta-preprocessor; no compiling and linking.',
+        'type'        : bool,
+        'default'     : False,
+    },
 )
 def build(parameters):
+
+
+
+    # Determine the files for the meta-preprocessor to scan through.
+
+    metapreprocessor_file_paths = [
+        pathlib.Path(root, file_name)
+        for root, directories, file_names in pxd.make_main_relative_path('./source').walk()
+        for                    file_name  in file_names
+        if file_name.endswith(('.c', '.h', '.py', '.ld', '.S'))
+    ]
+
+
+
+    # Begin meta-preprocessing!
+
+    try:
+        pxd.metapreprocess(
+            output_directory_path = pxd.make_main_relative_path('./electrical/meta'),
+            source_file_paths     = metapreprocessor_file_paths,
+        )
+    except pxd.MetaPreprocessorError:
+        sys.exit(1)
+
+
+
+    # Sometimes we only just need to run the meta-preprocessor without compiling.
+
+    if parameters.metapreprocess_only:
+        return
+
+
+
+    # Create the build artifact folder.
 
     pxd.execute_shell_command(
         bash = f'mkdir -p {pxd.make_main_relative_path('./build')}',
@@ -60,12 +103,26 @@ def build(parameters):
         ''',
     )
 
+
+
+    # Build the RayLib library.
+
     pxd.execute_shell_command('''
         cd ./deps/raylib && zig build
     ''')
 
+
+
+    # Build the executable.
+
     pxd.execute_shell_command('''
-        cd build && zig cc ../source/ThunkWeb.c ../deps/raylib/zig-out/lib/raylib.lib -l gdi32 -l Winmm -o ./ThunkWeb.exe
+        cd build &&
+            zig cc
+                ../source/ThunkWeb.c
+                ../deps/raylib/zig-out/lib/raylib.lib
+                -l gdi32
+                -l Winmm
+                -o ./ThunkWeb.exe
     ''')
 
 
